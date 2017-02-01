29ea82df5d1d0cc5c5f960ab6d5252167d853e36
Sandy Hogan <SVern99@aol.com>
2017-02-01 04:58:32 +0000

    Added user edit view and updated form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
