be54a12f318984bbcf3c39b05cea8be4eee2fb91
Sandy Hogan <SVern99@aol.com>
2017-02-02 20:44:39 +0000

    Completed user authentication in controllers and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -19,12 +22,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
     if @user.update(user_params)
-      flash[:success] = "Your account was updated successfully."
+      flash[:success] = "Your account was updated successfully #{@user.username}."
       redirect_to articles_path
     else
       render 'edit'
@@ -32,14 +35,24 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
     
 end
